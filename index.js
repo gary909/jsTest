3c1c95053daaf92b7bbbf13d66bbeebe1d5ef712
gary909 <garywhite61@hotmail.com>
2019-11-18 19:15:50 +0100

    hide box

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function welcome(){
     alert('Hello!');
     alert('Thanks for clicking');
 }
+
+function show(whichdiv){
+    document.getElementById(whichdiv).style.display="block";
+}
+
+function hide(whichdiv){
+    document.getElementById(whichdiv).style.display="none";
+}
